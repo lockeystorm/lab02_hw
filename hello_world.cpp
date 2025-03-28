9f344595daee66eeb5721867e21d79cf3e08bca8
lockeystorm <wallie4@ya.ru>
2025-03-29 00:18:43 +0300

    added comments to hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,8 +3,8 @@
 
 int main() {    
     std::string name;
-    std::cout << "What's your name? ";
-    getline(std::cin, name);
-    std::cout << "Hello world from " << name << std::endl;
+    std::cout << "What's your name? "; // ask user for his name
+    getline(std::cin, name); // accept user's name
+    std::cout << "Hello world from " << name << std::endl; // greeting
     return 0;
 }
