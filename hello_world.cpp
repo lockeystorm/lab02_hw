2f50f9c4d03f6afefbee3335f0fbb88060869c9a
lockeystorm <159271972+lockeystorm@users.noreply.github.com>
2025-03-30 20:08:36 +0300

    translate comments in hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,8 +3,8 @@
 
 int main() {    
     std::string name;
-    std::cout << "What's your name? "; // ask user for his name
-    getline(std::cin, name); // accept user's name
-    std::cout << "Hello world from " << name << std::endl; // greeting
+    std::cout << "What's your name? "; // спрашиваем имя пользователя
+    getline(std::cin, name); // считываем имя пользователя
+    std::cout << "Hello world from " << name << std::endl; // приветствие
     return 0;
 }
